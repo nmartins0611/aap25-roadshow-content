5e044900996b96a6a9d7c8310f2e845dd36045de
Nuno Martins <nmartins@redhat.com>
2024-12-19 11:16:12 +0200

    Update script.js

--- lab-resources/script.js
+++ lab-resources/script.js
@@ -1,8 +1,20 @@
-function playVideo(videoSrc, title) {
-  const video = document.getElementById("main-video");
-  const videoTitle = document.getElementById("video-title");
+// function playVideo(videoSrc, title) {
+//   const video = document.getElementById("main-video");
+//   const videoTitle = document.getElementById("video-title");
   
-  video.src = videoSrc;
-  videoTitle.textContent = title;
-  video.play();
+//   video.src = videoSrc;
+//   videoTitle.textContent = title;
+//   video.play();
+// }
+
+function playVideo(videoSrc, videoTitle) {
+  var videoElement = document.getElementById("main-video");
+  var titleElement = document.getElementById("video-title");
+
+  // Change the video source and title
+  videoElement.src = videoSrc;
+  titleElement.textContent = videoTitle;
+
+  // Play the video
+  videoElement.play();
 }
